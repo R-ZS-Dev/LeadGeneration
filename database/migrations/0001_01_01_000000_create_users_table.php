33f137a5ef09acf5a054b9557f0bca4e67d46261
samiansari39 <92986568+samiansari39@users.noreply.github.com>
2025-03-06 11:00:58 +0500

    create module fluid locations

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -24,6 +24,9 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable()->collation('utf8mb4_general_ci');
             $table->string('password')->collation('utf8mb4_general_ci');
             $table->rememberToken();
+            $table->string('insertby')->collation('utf8mb4_general_ci');
+            $table->enum('status', ['0', '1'])->default('1')->collation('utf8mb4_general_ci');
+            $table->enum('close', ['0', '1'])->default('1')->collation('utf8mb4_general_ci');
             $table->timestamps();
         });
 
---
+++ resources/views/config/fluid-location.blade.php
@@ -0,0 +1,253 @@
+@extends('sitemaster.master-layout')
+@section('title', 'All Fluid Locations')
+@section('content')
+    <div class="container-fluid">
+        <div class="row">
+
+            @if (session('error'))
+                <div class="alert alert-danger">
+                    {{ session('error') }}
+                </div>
+            @endif
+
+            <!-- Success Message -->
+            <div id="successMessage" class="alert alert-success" style="display: none;"></div>
+
+            <div id="signup-modal" class="modal fade" tabindex="-1" role="dialog" aria-hidden="true">
+                <div class="modal-dialog modal-dialog-centered">
+                    <div class="modal-content">
+
+                        <div class="modal-body">
+                            <div class="text-center mt-2 mb-4">
+                                <div class="d-flex justify-content-between align-items-center mt-2 mb-4">
+                                    <h4 class="mb-0"><b>Add Fluid Location</b></h4>
+                                    <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                        aria-label="Close"></button>
+                                </div>
+                            </div>
+
+                            <form method="POST" action="{{ route('add-fluid-location') }}" class="mt-4">
+                                @csrf
+                                <div class="row">
+                                    <div class="col-lg-12">
+                                        <div class="form-group mb-3">
+                                            <label for="">Fluid Location Name</label>
+                                            <input type="text" name="fl_name" id="" value="{{ old('fl_name') }}"
+                                                class="form-control" placeholder="Fluid Location Name">
+                                        </div>
+                                    </div>
+
+                                    <div class="col-lg-12">
+                                        <div class="form-group mb-3">
+                                            <label for="">Fluid Location Type</label>
+                                            <select name="fl_type" id="" class="form-control">
+                                                <option value="">Select Fluid Location Type</option>
+                                                <option value="External">External</option>
+                                                <option value="Patient">Patient</option>
+                                            </select>
+                                        </div>
+                                    </div>
+
+
+                                    <div class="col-lg-12">
+                                        <div class="form-group form-switch mb-3">
+                                            <input type="hidden" name="fl_active" value="0">
+                                            <input type="checkbox" role="switch" name="fl_active" id="active" checked
+                                                value="1" class="form-check-input"
+                                                {{ old('active') ? 'checked' : '' }}>
+                                            <label for="active" class="form-check-label">Active</label>
+
+                                        </div>
+                                    </div>
+                                    <div class="col-lg-12 text-center">
+                                        <button type="submit" class="btn w-100 btn-dark" id="submitBtn">Add
+                                            Fluid Location</button>
+                                    </div>
+                                </div>
+
+                            </form>
+                        </div>
+                    </div>
+                </div><!-- /.modal-content -->
+            </div><!-- /.modal-dialog -->
+        </div><!-- /.modal -->
+
+        <div class="col-12 mt-2">
+            <div class="card">
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col-lg-12 d-flex justify-content-end">
+
+                            <button type="button" class="btn waves-effect waves-light mb-2 btn-outline-primary"
+                                data-bs-toggle="modal" data-bs-target="#signup-modal">
+                                <i class="fas fa-plus"></i> Add Fluid Location
+                            </button>
+
+                        </div>
+                    </div>
+                    <div class="table-responsive">
+                        <table id="users-table" class="table table-striped table-bordered no-wrap">
+                            <thead>
+                                <tr>
+                                    <th>ID</th>
+                                    <th>Name</th>
+                                    <th>Type</th>
+                                    <th>Status</th>
+                                    <th>Action</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @php $i = 0; @endphp
+                                @foreach ($fluid as $index => $fl)
+                                    <tr id="row-{{ $fl->fl_id }}">
+                                        <td>{{ ++$i }}</td>
+                                        <td>{{ $fl->fl_name }}</td>
+                                        <td>{{ $fl->fl_type }}</td>
+                                        <td>
+                                            @if ($fl->fl_active == '1')
+                                                <span class="badge bg-success">Active</span>
+                                            @else
+                                                <span class="badge bg-danger">Inactive</span>
+                                            @endif
+                                        </td>
+                                        <td>
+                                            <a onclick="editEqg({{ json_encode($fl) }})" href="javascript:void(0);">
+                                                <i class="fa-solid fa-pen-to-square"></i>
+                                            </a>
+
+                                            <a href="javascript:void(0);"
+                                                onclick="confirmDelete('{{ route('delete-fluid-location', $fl->fl_id) }}', '{{ $fl->fl_id }}')"
+                                                class="edit-icon delete-user-btn text-danger">
+                                                <i class="fa-solid fa-trash-can-arrow-up"></i>
+                                            </a>
+                                        </td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    {{-- /* --------------------------- edit hospital modal -------------------------- */ --}}
+    <div id="editHospital" class="modal fade" tabindex="-1" role="dialog" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content ">
+                <div class="modal-body ">
+                    <div class="d-flex justify-content-between align-items-center mt-2 mb-4">
+                        <h4 class="mb-0"><b>Edit Fluid Location</b></h4>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <form method="POST" action="{{ route('edit-fluid-location') }}" class="mt-4">
+                        @csrf
+                        <input type="hidden" name="fl_id" id="fl_id">
+                        <div class="row">
+                            <div class="col-lg-12">
+                                <div class="form-group mb-3">
+                                    <label for="">Fluid Location Name</label>
+                                    <input type="text" name="fl_name" id="edit_name" value="{{ old('fl_name') }}"
+                                        class="form-control" placeholder="Fluid Location Name">
+                                </div>
+                            </div>
+
+                            <div class="col-lg-12">
+                                <div class="form-group mb-3">
+                                    <label for="">Fluid Location Type</label>
+                                    <select name="fl_type" id="edit_type" class="form-control">
+                                        <option value="">Select Fluid Location Type</option>
+                                        <option value="External">External</option>
+                                        <option value="Patient">Patient</option>
+                                    </select>
+                                </div>
+                            </div>
+
+
+                            <div class="col-lg-12">
+                                <div class="form-group form-switch mb-3">
+                                    <input type="hidden" name="fl_active" value="0">
+                                    <input type="checkbox" role="switch" name="fl_active" id="edit-active" checked
+                                        value="1" class="form-check-input" {{ old('active') ? 'checked' : '' }}>
+                                    <label for="edit-active" class="form-check-label">Active</label>
+
+                                </div>
+                            </div>
+                            <div class="col-lg-12 text-center">
+                                <button type="submit" class="btn w-100 btn-dark" id="submitBtn">Update
+                                    Fluid Location</button>
+                            </div>
+                        </div>
+
+                    </form>
+
+                </div>
+            </div><!-- /.modal-content -->
+        </div><!-- /.modal-dialog -->
+    </div>
+    </div>
+@endsection
+@section('script')
+    <script type="text/javascript">
+        $(document).ready(function() {
+            $('#users-table').DataTable({
+                "paging": true, // Enable pagination
+                "lengthChange": true, // Allow user to change the number of records per page
+                "searching": true, // Enable search functionality
+                "ordering": true, // Enable column sorting
+                "info": true, // Display info like "Showing 1 to 10 of 50 entries"
+                "autoWidth": false // Disable automatic column width adjustment
+            });
+        });
+    </script>
+    <script>
+        function editEqg(fl) {
+            document.getElementById("fl_id").value = fl.fl_id;
+            document.getElementById("edit_name").value = fl.fl_name;
+            document.getElementById("edit_type").value = fl.fl_type;
+            document.getElementById("edit-active").checked = fl.fl_active == 1;
+            var editModal = new bootstrap.Modal(document.getElementById("editHospital"));
+            editModal.show();
+        }
+    </script>
+
+
+    <script>
+        document.addEventListener("DOMContentLoaded", function() {
+            document.addEventListener("submit", function(event) {
+                const activeModal = document.querySelector(".modal.show");
+                if (!activeModal) return;
+
+                const form = activeModal.querySelector("form");
+                if (!form) return;
+
+                const inputs = form.querySelectorAll("input:not([type='hidden'])");
+                const submitBtn = form.querySelector("button[type='submit']");
+
+                let isValid = true;
+
+                inputs.forEach(input => {
+                    if (input.type !== "checkbox" && input.value.trim() === "") {
+                        input.classList.add("is-invalid");
+                        isValid = false;
+                    } else {
+                        input.classList.remove("is-invalid");
+                    }
+                });
+
+                if (!isValid) {
+                    event.preventDefault();
+                } else {
+                    submitBtn.innerHTML =
+                        '<span class="spinner-border spinner-border-sm"></span> Processing...';
+                    submitBtn.disabled = true;
+                }
+            });
+            document.addEventListener("input", function(event) {
+                const input = event.target;
+                if (input.value.trim() !== "") {
+                    input.classList.remove("is-invalid");
+                }
+            });
+        });
+    </script>
+@endsection
